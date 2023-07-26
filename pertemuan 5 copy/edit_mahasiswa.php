7ea067bd0be5c574e11fb8c6c56df972e4190c49
unknown <pnadila103@gmail.com>
2023-07-26 11:38:49 +0700

    Project Akhir

---
+++ pertemuan 5 copy/edit_mahasiswa.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Mahasiswa</title>
+</head>
+<body>
+<h1>Form Edit Mahasiswa</h1>
+    <a href="../pertemuan5/">Kembali</a>
+</body>
+</html>
